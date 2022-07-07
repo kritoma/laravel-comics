9e5de8d5724290e09394576eac620fe840da6eb0
kritoma <toma.kristian97@gmail.com>
2022-07-07 23:12:00 +0200

    aggiunto header + main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,24 @@
 @endsection
 
 @section('page-content')
-    
+    <section class="hero">
+    </section>
+    <section class="current_series">
+        <div class="container relative">
+            <button class="btn_current">CURRENT SERIES</button>
+            <ul class="cards">
+                @foreach ($comics as $elm) 
+                    <li class="card">
+                        <img src="{{$elm['thumb']}}" alt="{{$elm['title']}}">
+                        <h3>{{$elm['title']}}</h3>
+                    </li>
+                @endforeach 
+                </ul>
+            <div class="div_btn">
+                <button class="btn_load">LOAD MORE</button>
+            </div>
+        </div>
+      
+        
+    </section>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,7 +7,29 @@
             </div>
         </div>
     </div>
-    <div class="header_bottom">
-
+    <div class="container">
+        <div class="header_bottom">
+            <div class="logo">
+                <img src="{{asset('img/dc-logo.png')}}" alt="logo DC">
+            </div>
+            <nav>
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEO</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </nav>
+            <div class="search_bar">
+                <input type="text" placeholder="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </div>
     </div>
 </header>
