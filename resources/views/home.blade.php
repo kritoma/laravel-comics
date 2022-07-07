602affd7d29163590dad0ce0ec782608589638ee
kritoma <toma.kristian97@gmail.com>
2022-07-07 23:28:40 +0200

    completato main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,8 +21,32 @@
             <div class="div_btn">
                 <button class="btn_load">LOAD MORE</button>
             </div>
+        </div>   
+    </section>
+    <section class="digital_comics">
+        <div class="container">
+            <ul>
+                <li>
+                    <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="digital comics">
+                    <h5>DIGITAL COMICS</h5>
+                </li>
+                <li>
+                    <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="merchandise">
+                    <h5>DC MERCHANDISE</h5>
+                </li>
+                <li>
+                    <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="subscription">
+                    <h5>SUBSCRIPTION</h5>
+                </li>
+                <li>
+                    <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="comic shop locator">
+                    <h5>COMIC SHOP LOCATOR</h5>
+                </li>
+                <li>
+                    <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="dc power visa">
+                    <h5>DC POWER VISA</h5>
+                </li>
+            </ul>
         </div>
-      
-        
     </section>
 @endsection
