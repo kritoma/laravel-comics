b1ca3acc02f44e83a10657f4bfd3cc1e16f52ea2
kritoma <toma.kristian97@gmail.com>
2022-07-07 16:50:18 +0200

    .

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('page-title', 'DC Comics')</title>
-    <link rel="stylesheet" href="{{assets('ccs/app.css')}}">
+    <link rel="stylesheet" href="{{asset('ccs/app.css')}}">
 </head>
 <body>
     @include('partials.header')
