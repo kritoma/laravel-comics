a64dfaecb72c69abc0ff3b062cc3c4549f44089c
kritoma <toma.kristian97@gmail.com>
2022-07-07 23:45:22 +0200

    aggiunto footer_bottom

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,22 @@
+<footer>
+    <div class="footer_top">
+        <div class="container">
+
+        </div>
+    </div>
+    <div class="footer_bottom">
+        <div class="container">
+            <div class="social"> 
+                <button>SIGN-UP NOW!</button>
+                <div class="icons">
+                    <div>FOLLOW US</div>
+                    <img src="{{asset('img/footer-facebook.png')}}" alt="facebook">
+                    <img src="{{asset('img/footer-twitter.png')}}" alt="twitter">
+                    <img src="{{asset('img/footer-youtube.png')}}" alt="youtube">
+                    <img src="{{asset('img/footer-pinterest.png')}}" alt="pinterest">
+                    <img src="{{asset('img/footer-periscope.png')}}" alt="periscope">
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
