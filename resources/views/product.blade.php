50e7c6c1a3d4258e9f1507b4aac85728a2e87a61
kritoma <toma.kristian97@gmail.com>
2022-07-12 22:24:52 +0200

    completato esercizio

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -43,22 +43,26 @@
                         <h3>Talent</h3>
                         <div class="tablet">
                             <div>Art by:</div>
-                            @foreach ($prodotto['artists'] as $artist)
-                                <span>{{$artist}}</span>
-                            @endforeach
+                            <div class="blue">
+                                @foreach ($prodotto['artists'] as $artist)
+                                    <span>{{$artist}},</span>
+                                @endforeach
+                            </div>
                         </div>
                         <div class="tablet">
                             <div>written by:</div>
-                            @foreach ($prodotto['writers'] as $writer)
-                                <span>{{$writer}},</span>
-                            @endforeach
+                            <div class="blue">
+                                @foreach ($prodotto['writers'] as $writer)
+                                    <span>{{$writer}},</span>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                     <div class="specs">
                         <h3>Specs</h3>
                         <div class="tablet">
                             <div>Series:</div>
-                            <div>{{$prodotto['series']}}</div>
+                            <div class="blue">{{$prodotto['series']}}</div>
                         </div>
                         <div class="tablet">
                             <div>U.S. Price:</div>
